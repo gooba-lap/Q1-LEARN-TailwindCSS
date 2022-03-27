6f6a4cc60c754c7e00aab894282768720a855172
wasinkp <73060136+SinsamutQ@users.noreply.github.com>
2022-03-27 23:41:03 +0700

    success 🍻

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Q1-LEARN-TailwindCSS
+## Q1-LEARN-TailwindCSS
+### Build A React Website With Tailwind CSS - Learn React & Tailwind CSS Basics
+- https://www.youtube.com/watch?v=xG1K3vl_MC8 
--- blockchin-react-tailwind/src/App.js
+++ blockchin-react-tailwind/src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import About from "./components/About";
+import Developer from "./components/Developer";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Subscribe from "./components/Subscribe";
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <Hero />
+      <About />
+      <Developer />
+      <Subscribe />
+      <Footer />
     </div>
   );
 }
